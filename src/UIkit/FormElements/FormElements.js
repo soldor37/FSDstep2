fdc304a7ceea7ccd247218b0eabeb7a40862f3a0
soldor37 <seventhpirate@gmail.com>
2020-07-09 12:19:29 +0300

    dropdown arrow toggle

--- src/UIkit/FormElements/FormElements.js
+++ src/UIkit/FormElements/FormElements.js
@@ -2,5 +2,8 @@ import FormElements from './FormElements.scss'
 
 
 $(function(){
-    
+    //меняет цвет стрелки при раскрытии дропдауна
+    $('.dropdown__input').click(()=>{
+        $('.dropdown .dropdown__arrow').toggleClass('dropdown__arrow_focus')
+    })
 })
