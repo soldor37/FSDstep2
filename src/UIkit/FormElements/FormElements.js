ccd49265e029e373bd8238247d8c0fa33b3b2810
soldor37 <seventhpirate@gmail.com>
2020-07-16 20:23:11 +0300

    1st block on formElem done

--- src/UIkit/FormElements/FormElements.js
+++ src/UIkit/FormElements/FormElements.js
@@ -11,7 +11,7 @@ import 'air-datepicker/dist/js/datepicker'
 $(function(){
     //меняет цвет стрелки при раскрытии дропдауна
     //TODO: пофиксить и доделать
-    $('.dropdown__input').click(()=>{
-        $('.dropdown .dropdown__arrow').toggleClass('dropdown__arrow_focus')
-    })
+    // $('.dropdown__input').click(()=>{
+    //     $('.dropdown .dropdown__arrow').toggleClass('dropdown__arrow_focus')
+    // })
 })
