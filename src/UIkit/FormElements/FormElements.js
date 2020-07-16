009d49a22789a66dfa0e6f04ad044df31ad13931
soldor37 <seventhpirate@gmail.com>
2020-07-16 12:12:36 +0300

    filter date drop init

--- src/UIkit/FormElements/FormElements.js
+++ src/UIkit/FormElements/FormElements.js
@@ -9,6 +9,7 @@ import 'air-datepicker/dist/js/datepicker'
 
 $(function(){
     //меняет цвет стрелки при раскрытии дропдауна
+    //TODO: пофиксить и доделать
     $('.dropdown__input').click(()=>{
         $('.dropdown .dropdown__arrow').toggleClass('dropdown__arrow_focus')
     })
